4360d32f3b82918d6c52911e136bb13561ce7514
Maximo <maxmonteiro86@gmail.com>
2019-04-29 13:45:45 -0300

    Alterando proteção das requisições JavaScript nas rotas API em vez do Controller

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::apiResources([
+        'user' => 'API\UserController'
+    ]);
+});
+/*
 Route::apiResources([
     'user' => 'API\UserController'
-]);
+]); */
